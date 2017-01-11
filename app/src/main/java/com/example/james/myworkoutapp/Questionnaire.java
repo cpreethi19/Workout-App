20986b725229af9697c0d097578190399d6cc6ce
James <jameslezadabonifacio@gmail.com>
2017-01-10 22:06:24 -0500

    First commit

---
+++ app/src/main/java/com/example/james/myworkoutapp/Questionnaire.java
@@ -0,0 +1,13 @@
+package com.example.james.myworkoutapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Questionnaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionnaire);
+    }
+}
